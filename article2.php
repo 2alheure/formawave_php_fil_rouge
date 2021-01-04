80ce2b17ac86b865e99bfd96788c928b1b333a42
2alheure <jojo77.juv@live.fr>
2021-01-04 15:26:33 +0100

    Article layout + updating layout with id and DB

--- article2.php
+++ article2.php
@@ -1,19 +1,3 @@
 <?php
-include 'functions.php';
-include './db_functions_with_pdo.php';
-$selected_article = getArticle(connectDB(), 2);
-
-$titre = $selected_article['titre'] . ' | Mon super blog';
-
-include 'header.php';
-?>
-
-
-<img src="<?= $selected_article['image'] ?>" alt="<?= $selected_article['image_alt'] ?>" class="banner" />
-<small><?= $selected_article['image_copyright'] ?></small>
-
-<h1 class="mb-4"><?= $selected_article['titre'] ?></h1>
-
-<p><?= $selected_article['contenu'] ?></p>
-
-<?php include 'footer.php';
+$article_id = 2;
+include './article_layout.php';
---
+++ article_layout.php
@@ -0,0 +1,19 @@
+<?php
+include 'functions.php';
+include './db_functions_with_pdo.php';
+$selected_article = getArticle(connectDB(), $article_id);
+
+$titre = $selected_article['titre'] . ' | Mon super blog';
+
+include 'header.php';
+?>
+
+
+<img src="<?= $selected_article['image'] ?>" alt="<?= $selected_article['image_alt'] ?>" class="banner" />
+<small><?= $selected_article['image_copyright'] ?></small>
+
+<h1 class="mb-4"><?= $selected_article['titre'] ?></h1>
+
+<p><?= $selected_article['contenu'] ?></p>
+
+<?php include 'footer.php';
