6da4241d350ba51bafac5becf70133e75e29704f
2alheure <jojo77.juv@live.fr>
2021-01-04 10:12:03 +0100

    PDO's + mysqli's DB function into their files

--- db_functions_with_mysqli.php
+++ db_functions_with_mysqli.php
@@ -1,6 +1,6 @@
 <?php
 
-function connectDBWithMysqli()
+function connectDB()
 {
     require 'config_db.php';
     $bdd =  new mysqli($host, $user, $password, $dbname);
@@ -10,4 +10,56 @@ function connectDBWithMysqli()
     }
 
     return $bdd;
+}
+
+/**
+ * Si on fournit id, 
+ * alors on cherche UN article qui a cet id
+ * 
+ * Sinon, on prend TOUS les articles
+ */
+function getArticle($bdd, $id = null)
+{
+    if (empty($id)) {
+        // Tous les articles
+        $query = $bdd->query('SELECT * FROM articles');
+
+        if ($query->num_rows == 0) {
+            return false;
+        } else {
+            return $query->fetch_all(MYSQLI_ASSOC);
+        }
+    } else {
+        // Un seul article
+
+        if (is_numeric($id)) {
+            $stmt = $bdd->prepare('SELECT * FROM articles WHERE id = ?');
+            $stmt->bind_param('i', $id);
+            $result = $stmt->execute();
+
+            if (!$result) {
+                return false;
+            }
+
+            $stmt->bind_result($article_id, $titre, $date, $contenu, $lien, $image, $image_alt, $image_copyright);
+
+
+            if (!($stmt->fetch())) {
+                return false;
+            } else {
+                return array(
+                    'id' => $article_id,
+                    'titre' => $titre,
+                    'date' => $date,
+                    'contenu' => $contenu,
+                    'lien' => $lien,
+                    'image' => $image,
+                    'image_alt' => $image_alt,
+                    'image_copyright' => $image_copyright,
+                );
+            }
+        } else {
+            die('Erreur lors de la tentative de récupération d\'un article.');
+        }
+    }
 }
--- db_functions_with_pdo.php
+++ db_functions_with_pdo.php
@@ -1,6 +1,6 @@
 <?php
 
-function connectDBWithPDO()
+function connectDB()
 {
     require 'config_db.php';
     try {
@@ -10,4 +10,39 @@ function connectDBWithPDO()
     }
 
     return $bdd;
+}
+
+/**
+ * Si on fournit id, 
+ * alors on cherche UN article qui a cet id
+ * 
+ * Sinon, on prend TOUS les articles
+ */
+function getArticle($bdd, $id = null)
+{
+    if (empty($id)) {
+        // Tous les articles
+        $query = $bdd->query('SELECT * FROM articles');
+
+        if ($query->rowCount() == 0) {
+            return false;
+        } else {
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+    } else {
+        // Un seul article
+
+        if (is_numeric($id)) {
+            $stmt = $bdd->prepare('SELECT * FROM articles WHERE id = ?');
+            $verif = $stmt->execute(array($id));
+
+            if (!$verif || !($article = $stmt->fetch(PDO::FETCH_ASSOC))) {
+                return false;
+            } else {
+                return $article;
+            }
+        } else {
+            die('Erreur lors de la tentative de récupération d\'un article : id doit être un nombre.');
+        }
+    }
 }
