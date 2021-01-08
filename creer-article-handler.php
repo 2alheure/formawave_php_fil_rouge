2365a7b5a68cc57d78a52d937d9bb3cfce25e77a
2alheure <jojo77.juv@live.fr>
2021-01-08 18:27:24 +0100

    handle article post and DB insert

---
+++ creer-article-handler.php
@@ -0,0 +1,22 @@
+<?php
+
+include './db_functions_with_pdo.php';
+$bdd = connectDB();
+
+// On récupère les données de notre formulaire
+$article = array(
+    'titre' => $_POST['titre'],
+    'contenu' => $_POST['contenu'],
+    'image' => $_POST['image'],
+    'image_alt' => $_POST['image_alt'],
+    'image_copyright' => $_POST['image_copyright'],
+);
+
+if (insertArticle($bdd, $article)) {
+    // Si la requête a réussi, on renvoie vers la page du nouvel article
+    // lastInsertId permet de récupérer... Le dernier id inséré ;-)
+    header('location: article.php?id=' . $bdd->lastInsertId());
+} else {
+    // Sinon on renvoie vers le formulaire (qui indique une erreur ;-)
+    header('location: creer-article.php?error=true');
+}
